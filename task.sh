2f86920ee34efa1d02e49ac3eebadcf9ca22ca57
H. Watanabe <kaityo@users.sourceforge.jp>
2021-12-20 22:52:01 +0900

    adds task.sh

---
+++ task.sh
@@ -0,0 +1,11 @@
+# tasks
+python3 hello.py < seed0.dat > output0.dat
+python3 hello.py < seed1.dat > output1.dat
+python3 hello.py < seed2.dat > output2.dat
+python3 hello.py < seed3.dat > output3.dat
+python3 hello.py < seed4.dat > output4.dat
+python3 hello.py < seed5.dat > output5.dat
+python3 hello.py < seed6.dat > output6.dat
+python3 hello.py < seed7.dat > output7.dat
+python3 hello.py < seed8.dat > output8.dat
+python3 hello.py < seed9.dat > output9.dat
